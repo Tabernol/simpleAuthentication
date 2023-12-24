9912d67af557c9315363210842ad1f8886883073
MaxKrasnoloskyi <makskrasnopolskyi@gmail.com>
2023-12-24 11:29:38 +0200

    Initial commit

---
+++ src/test/java/com/krasnoposlkyi/simpleauthentication/SimpleAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krasnoposlkyi.simpleauthentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleAuthenticationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
